26c054933118062d3a9017d218873843f19768a1
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:49:25 +0400

    Email Service Completed

---
+++ First For Mvc Project/Services/Concretes/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using First_For_Mvc_Project.Contracts.Email;
+
+namespace First_For_Mvc_Project.Services.Concretes
+{
+    public interface IEmailService
+    {
+        public void Send(MessageDto messageDto);
+    }
+}
---
+++ First For Mvc Project/Services/Services/SMTPService.cs
@@ -0,0 +1,64 @@
+﻿using First_For_Mvc_Project.Contracts.Email;
+using First_For_Mvc_Project.Options;
+using First_For_Mvc_Project.Services.Concretes;
+using MailKit.Net.Smtp;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace First_For_Mvc_Project.Services.Services
+{
+    public class SMTPService : IEmailService
+    {
+        private EmailConfigOptions _emailConfig;
+
+        public SMTPService(IOptions<EmailConfigOptions> emailConfigOptions)
+        {
+            _emailConfig = emailConfigOptions.Value;
+        }
+
+        public void Send(MessageDto message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(MessageDto message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(string.Empty, _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = message.Content
+            };
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage emailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+
+                    client.Send(emailMessage);
+                }
+                catch
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+    }
+}
