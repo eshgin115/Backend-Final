facd52550e1c92dff0e312202a6990976584cbce
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:44:50 +0400

    Add contracts

---
+++ First For Mvc Project/Contracts/Product/Manage.cs
@@ -0,0 +1,9 @@
+﻿namespace First_For_Mvc_Project.Contracts.Product
+{
+    public class Manage
+    {
+        public const string NEW_PRODUCT = "NewProduct";
+        public const string TOP_PRODUCT = "TopProduct";
+        public const string Latest_PRODUCT = "LatestProduct";
+    }
+}
