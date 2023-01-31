3677d874b5a19263d5a1be883248bb3f499f7a99
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:52:50 +0400

    Add atributs

---
+++ First For Mvc Project/Attributs/IsAuthenticated.cs
@@ -0,0 +1,26 @@
+﻿using First_For_Mvc_Project.Services.Concretes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace First_For_Mvc_Project.Attributs
+{
+
+    public class IsAuthenticated : ActionFilterAttribute, IActionFilter
+    {
+       
+      
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var userService = filterContext.HttpContext.RequestServices.GetRequiredService<IUserService>();
+            if (userService.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                    { "controller", "account" },
+                    { "action", "dashboard" }
+                });
+            }
+        }
+    }
+}
