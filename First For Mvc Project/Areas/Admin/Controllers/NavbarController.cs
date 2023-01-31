b49901705699edac14be34b52de3e6f1a077ad8e
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:56:35 +0400

    Navbar Controller with SubNavbar completed

---
+++ First For Mvc Project/Areas/Admin/Controllers/NavbarController.cs
@@ -0,0 +1,164 @@
+﻿using First_For_Mvc_Project.Areas.Admin.ViewModels.Navbar;
+using First_For_Mvc_Project.Database;
+using First_For_Mvc_Project.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace First_For_Mvc_Project.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("navbar")]
+    public class NavbarController :Controller
+    {
+        private readonly DataContext _dataContext;
+        private readonly ILogger<NavbarController> _logger;
+
+        public NavbarController(DataContext dataContext, ILogger<NavbarController> logger)
+        {
+            _dataContext = dataContext;
+            _logger = logger;
+        }
+        #region List
+        [HttpGet("list", Name = "admin-navbar-list")]
+        public async Task<IActionResult> ListAsync()
+        {
+            var model = await _dataContext.Navbars
+                .Select(n => new ListViewModel(n.Id, n.Name,n.Order))
+                .ToListAsync();
+
+            return View(model);
+        }
+        #endregion
+
+
+        #region add
+        [HttpGet("add", Name = "admin-navbar-add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost("add", Name = "admin-navbar-add")]
+        public async Task<IActionResult> AddAsync(AddViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+
+
+
+
+            if (_dataContext.Navbars.Any(n => n.Order == model.Order))
+            {
+                ModelState.AddModelError(String.Empty, "this order using");
+                return View(model);
+
+            }
+            var navbar = CreateNavbar();
+
+            await _dataContext.Navbars.AddAsync(navbar);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToRoute("admin-navbar-list");
+
+            Navbar CreateNavbar()
+            {
+                var navbar = new Navbar
+                {
+                    Name = model.Name,
+                    ToURL = model.ToURL,
+                    Order = model.Order,
+                    IsViewOnFooter = model.IsViewOnFooter,
+                    IsViewOnHeader = model.IsViewOnHeader,
+
+                };
+                return navbar;
+            };
+        }
+        #endregion
+
+
+        #region update
+        [HttpGet("update/{id}", Name = "admin-navbar-update")]
+        public async Task<IActionResult> UpdateAsync([FromRoute] int id)
+        {
+            var navbar = await _dataContext.Navbars.FirstOrDefaultAsync(n => n.Id == id);
+
+
+            if (navbar is null) return NotFound();
+
+
+
+
+            var model = CreateModel();
+
+            return View( model);
+
+            UpdateViewModel CreateModel()
+            {
+                var model = new UpdateViewModel
+                {
+                    Order = navbar.Order,
+                    IsViewOnHeader = navbar.IsViewOnHeader,
+                    IsViewOnFooter = navbar.IsViewOnFooter,
+                    Id = id
+                };
+                return model;
+            }
+
+
+        }
+        [HttpPost("update/{id}", Name = "admin-navbar-update")]
+        public async Task<IActionResult> UpdateAsync(UpdateViewModel model)
+        {
+            var navbar = await _dataContext.Navbars.FirstOrDefaultAsync(n => n.Id == model.Id);
+            if (navbar is null) return NotFound();
+
+
+
+
+
+            if (!ModelState.IsValid) return View( model);
+
+
+
+
+            if (!_dataContext.Navbars.Any(n => n.Id == model.Id) && !(model.Order == navbar.Order)) return View( model);
+
+
+
+
+            Update();
+
+
+            await _dataContext.SaveChangesAsync();
+
+            return RedirectToRoute("admin-navbar-list");
+
+            void Update()
+            {
+                navbar.Order = model.Order;
+                navbar.ToURL = model.ToURL;
+                navbar.Name = model.Name;
+                navbar.IsViewOnFooter = model.IsViewOnFooter;
+                navbar.IsViewOnHeader = model.IsViewOnHeader;
+            }
+
+        }
+        #endregion
+        #region delete
+        [HttpPost("delete/{id}", Name = "admin-navbar-delete")]
+        public async Task<IActionResult> DeleteAsync(UpdateViewModel model)
+        {
+            var navbar = await _dataContext.Navbars.FirstOrDefaultAsync(n => n.Id == model.Id);
+            if (navbar is null) return NotFound();
+
+
+            _dataContext.Navbars.Remove(navbar);
+            await _dataContext.SaveChangesAsync();
+
+
+
+            return RedirectToRoute("admin-navbar-list");
+
+        }
+        #endregion
+    }
+}
---
+++ First For Mvc Project/Areas/Admin/Controllers/SubnavbarController.cs
@@ -0,0 +1,186 @@
+﻿using First_For_Mvc_Project.Areas.Admin.ViewModels.Subnavbar;
+using First_For_Mvc_Project.Database;
+using First_For_Mvc_Project.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace First_For_Mvc_Project.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("subnavbar")]
+    public class SubnavbarController : Controller
+    {
+
+        private readonly DataContext _dataContext;
+        private readonly ILogger<SubnavbarController> _logger;
+        public SubnavbarController(DataContext dataContext, ILogger<SubnavbarController> logger)
+        {
+            _dataContext = dataContext;
+            _logger = logger;
+        }
+
+        [HttpGet("list", Name = "admin-subnavbar-list")]
+        public async Task<IActionResult> List()
+        {
+            var model = await _dataContext.Subnavbars.Select(s => new ListViewModel(s.Id, s.Name, s.Navbar.Name)).ToListAsync();
+            return View(model);
+        }
+
+        #region Add
+        [HttpGet("add", Name = "admin-subnavbar-add")]
+        public async Task<IActionResult> AddAsync()
+        {
+            var model = new AddViewModel
+            {
+
+                Navbars = await _dataContext.Navbars.Select(n => new ViewModels.Navbar.ListViewModel(n.Id, n.Name)).ToListAsync()
+
+            };
+            return View(model);
+        }
+        [HttpPost("add", Name = "admin-subnavbar-add")]
+        public async Task<IActionResult> Add(AddViewModel model)
+        {
+            if (!ModelState.IsValid) return await GetView(model);
+
+            if (!_dataContext.Navbars.Any(n => n.Id == model.NavbarId))
+            {
+                ModelState.AddModelError(String.Empty, "Navbar is not found");
+                return await GetView(model);
+            }
+
+
+
+
+            await AddBook();
+
+            return RedirectToRoute("admin-subnavbar-list");
+
+
+
+
+            async Task<IActionResult> GetView(AddViewModel model)
+            {
+                model.Navbars = await _dataContext.Navbars
+                    .Select(s => new ViewModels.Navbar.ListViewModel(s.Id, s.Name))
+                    .ToListAsync();
+
+
+                return View(model);
+            }
+
+            async Task AddBook()
+            {
+                var subnavbar = new Subnavbar
+                {
+                    Name = model.Name,
+                    ToURL = model.ToURL,
+                    NavbarId = model.NavbarId,
+                    Order = model.Order,
+
+
+                };
+
+                await _dataContext.Subnavbars.AddAsync(subnavbar);
+
+
+
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+
+        #endregion
+        #region Update
+        [HttpGet("update/{id}", Name = "admin-subnavbar-update")]
+        public async Task<IActionResult> UpdateAsync([FromRoute] int id)
+        {
+            var subnavbar = await _dataContext.Subnavbars.FirstOrDefaultAsync(n => n.Id == id);
+            if (subnavbar is null) return NotFound();
+
+            var model = await CreateModelAsync();
+
+            return View(model);
+
+
+            async Task<UpdateViewModel> CreateModelAsync()
+            {
+                var model = new UpdateViewModel
+                {
+                    Id = subnavbar.Id,
+                    Order = subnavbar.Order,
+                    ToURL = subnavbar.ToURL,
+                    Name = subnavbar.Name,
+                    Navbars = await _dataContext.Navbars.Select(s => new ViewModels.Navbar.ListViewModel(s.Id, s.Name)).ToListAsync()
+
+
+                };
+                return model;
+            }
+        }
+
+
+        [HttpPost("update/{id}", Name = "admin-subnavbar-update")]
+        public async Task<IActionResult> UpdateAsync(UpdateViewModel model)
+        {
+            var subnavbar = await _dataContext.Subnavbars.FirstOrDefaultAsync(b => b.Id == model.Id);
+            if (subnavbar is null) return NotFound();
+
+
+            if (!ModelState.IsValid) return await GetView(model);
+
+
+            if (!_dataContext.Navbars.Any(n => n.Id == model.NavbarId))
+            {
+                ModelState.AddModelError(String.Empty, "navbar is not found");
+                return await GetView(model);
+            }
+
+
+
+
+            await UpdateBookAsync();
+
+            return RedirectToRoute("admin-subnavbar-list");
+
+
+
+
+            async Task<IActionResult> GetView(UpdateViewModel model)
+            {
+                model.Navbars = await _dataContext.Navbars.Select(s => new ViewModels.Navbar.ListViewModel(s.Id, s.Name)).ToListAsync();
+
+
+
+                return View(model);
+            }
+
+            async Task UpdateBookAsync()
+            {
+                subnavbar.ToURL = model.ToURL;
+                subnavbar.NavbarId = model.NavbarId;
+                subnavbar.Order = model.Order;
+                subnavbar.Name = model.Name;
+                await _dataContext.SaveChangesAsync();
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        [HttpPost("delete/{id}", Name = "admin-subnavbar-delete")]
+        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        {
+            var subnavbar = await _dataContext.Subnavbars.FirstOrDefaultAsync(b => b.Id == id);
+            if (subnavbar is null) return NotFound();
+
+
+            _dataContext.Subnavbars.Remove(subnavbar);
+            await _dataContext.SaveChangesAsync();
+
+            return RedirectToRoute("admin-subnavbar-list");
+        }
+
+        #endregion
+    }
+}
