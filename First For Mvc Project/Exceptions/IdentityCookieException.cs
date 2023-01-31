67c6aa00d7e533866d3de636446abd34885f5922
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:53:24 +0400

    CookieException add

---
+++ First For Mvc Project/Exceptions/IdentityCookieException.cs
@@ -0,0 +1,11 @@
+﻿namespace First_For_Mvc_Project.Exceptions
+{
+    public class IdentityCookieException : ApplicationException
+    {
+        public IdentityCookieException(string? message)
+           : base(message)
+        {
+
+        }
+    }
+}
