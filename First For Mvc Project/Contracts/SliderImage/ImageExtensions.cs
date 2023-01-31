0c2f57ecf186f4fb66a433b624327420e0f786dc
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:41:57 +0400

    Add Concrats for file

---
+++ First For Mvc Project/Contracts/SliderImage/ImageExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace First_For_Mvc_Project.Contracts.SliderImage
+{
+    public enum FileExtensions
+    {
+        JPG = 1,
+        PNG = 2,
+        JPEG = 4,
+        MP4=8,
+    }
+
+    public static class FileExtensionExtensions
+    {
+        public static string GetExtension(this FileExtensions fileExtensions)
+        {
+            switch (fileExtensions)
+            {
+                case FileExtensions.JPG:
+                    return $".{nameof(FileExtensions.JPG).ToLowerInvariant()}";
+                case FileExtensions.PNG:
+                    return $".png";
+                case FileExtensions.JPEG:
+                    return $".jpg";
+                case FileExtensions.MP4:
+                    return $".mp4";
+                default:
+                    throw new Exception("This extension not found");
+            }
+        }
+    }
+}
---
+++ First For Mvc Project/Contracts/SliderImage/SliderImageType.cs
@@ -0,0 +1,10 @@
+﻿namespace First_For_Mvc_Project.Contracts.SliderImage
+{
+    public enum SliderImageType
+    {
+        None=0,
+        Main = 1,
+        Hover = 2,
+        Detail = 4
+    }
+}
