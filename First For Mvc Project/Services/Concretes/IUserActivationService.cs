9c0665a827ba85c519df193dd3ae13bd787ab3e7
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:50:07 +0400

    UserActivation Completed

---
+++ First For Mvc Project/Services/Concretes/IUserActivationService.cs
@@ -0,0 +1,9 @@
+﻿using First_For_Mvc_Project.Database.Models;
+
+namespace First_For_Mvc_Project.Services.Concretes
+{
+    public interface IUserActivationService
+    {
+        Task SendActivationUrlAsync(User user);
+    }
+}
