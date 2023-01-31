fe300a9c67bb45067d7ce2245b21d0abf733d204
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:30:39 +0400

    User DomenModel

---
+++ First For Mvc Project/Database/Models/Basket.cs
@@ -0,0 +1,15 @@
+﻿using First_For_Mvc_Project.Database.Models.Common;
+
+namespace First_For_Mvc_Project.Database.Models
+{
+    public class Basket : BaseEntity<int>, IAuditable
+    {
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public List<BasketProduct>? BasketProducts { get; set; }
+    }
+}
---
+++ First For Mvc Project/Database/Models/UserActivation.cs
@@ -0,0 +1,17 @@
+﻿using First_For_Mvc_Project.Database.Models.Common;
+
+namespace First_For_Mvc_Project.Database.Models
+{
+    public class UserActivation : BaseEntity<int>, IAuditable
+    {
+        public string? ActivationUrl { get; set; }
+        public string? ActivationToken { get; set; }
+        public DateTime ExpireDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
