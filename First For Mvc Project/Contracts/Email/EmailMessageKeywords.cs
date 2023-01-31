bcdac92843bdb820cc84d3f5a6390d1113d88b8b
farajov ewqin <eshqin115@gmail.com>
2023-01-31 19:39:25 +0400

    Add Email concrats

---
+++ First For Mvc Project/Contracts/Email/EmailMessageKeywords.cs
@@ -0,0 +1,7 @@
+﻿namespace First_For_Mvc_Project.Contracts.Email
+{
+    public class EmailMessageKeywords
+    {
+        public const string ACTIVATION_URL = "{activation_url}";
+    }
+}
---
+++ First For Mvc Project/Contracts/Email/EmailMessages.cs
@@ -0,0 +1,16 @@
+﻿namespace First_For_Mvc_Project.Contracts.Email
+{
+    public class EmailMessages
+    {
+        public static class Subject
+        {
+            public const string ACTIVATION_MESSAGE = $"Hesabin aktivlesdirilmesi";
+            public const string ORDER_MESSAGE = $"Order notification";
+        }
+
+        public static class Body
+        {
+            public const string ACTIVATION_MESSAGE = $"Sizin activation urliniz : {EmailMessageKeywords.ACTIVATION_URL}";
+        }
+    }
+}
