6ddc8a966017479988cc7d5e1164405eed7f69a8
farajov ewqin <eshqin115@gmail.com>
2023-02-01 14:01:11 +0400

    add migration videourlfrombrauser

--- First For Mvc Project/Migrations/20230201100044_blog_VideoUrlFromBrauser.Designer.cs
+++ First For Mvc Project/Migrations/20230201100044_blog_VideoUrlFromBrauser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pronia.Database;
 
@@ -11,9 +12,10 @@ using Pronia.Database;
 namespace Pronia.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230201100044_blog_VideoUrlFromBrauser")]
+    partial class blog_VideoUrlFromBrauser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ First For Mvc Project/Migrations/20230201100044_blog_VideoUrlFromBrauser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pronia.Migrations
+{
+    public partial class blog_VideoUrlFromBrauser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoURLFromBrauser",
+                table: "BlogVideos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoURLFromBrauser",
+                table: "BlogVideos");
+        }
+    }
+}
